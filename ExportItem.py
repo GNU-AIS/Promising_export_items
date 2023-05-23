6278501ba1413397c3b840434ebc2a0a1ddcfb82
Juyoung_Laptop <ymail3@naver.com>
2023-05-23 21:25:43 +0900

    asd

---
+++ ExportItem.py
@@ -0,0 +1,10 @@
+class Item:
+    def __init__(self, balPayments, expDir, expWgt, hsCode, impDir, impWgt, statKor, year):
+        self.balPayments = balPayments
+        self.expDir = expDir
+        self.expWgt = expWgt
+        self.hsCode = hsCode
+        self.impDir = impDir
+        self.impWgt = impWgt
+        self.statKor = statKor
+        self.year = year
