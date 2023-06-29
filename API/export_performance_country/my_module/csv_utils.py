ce040630240a50c1b42d4219122cac09f4221e63
wndudwkd003 <ymail3@naver.com>
2023-06-30 00:29:11 +0900

    품목별 국가별 API 호출 API 완성

--- API/export_performance_country/my_module/csv_utils.py
+++ API/export_performance_country/my_module/csv_utils.py
@@ -1,15 +1,32 @@
 """ 리스트를 받아서 csv로 저장 """
 
-import pandas as pd
 import openpyxl
+import csv
+from tqdm import tqdm
 
 
-def write_csv(items: list, path_name: str):
-    """ 리스트와 경로를 받아서 csv 파일로 저장 """
+def write_csv(items, file_name):
+    """
+    이터레이터와 파일의경로를 받아서 csv 파일로 저장한다
+    pandas를 쓰지 않는 이유는 메모리 공간을 효율적으로 쓰끼 위해
 
-    with open(path_name, 'w', newline='', encoding='utf-8-sig') as file:
-        df = pd.DataFrame(items,
-                          columns=['year', 'statCdCntnKor1', 'statCd', 'statKor', 'hsCd', 'expWgt', 'expDlr', 'impWgt',
-                                   'impDlr', 'balPayments'])
+    :param items: 파일의 데이터가될 이터레이터
+    :param file_name: 파일 주소를 포함하는 파일 이름
+    """
 
-        df.to_csv(file, index=True, encoding='utf-8-sig')
+    header_flag = 0  # 처음 반복에서만 헤더를 추가함
+
+    file = open(file_name, 'a', newline='', encoding='utf-8-sig')
+
+    # 이터레이터를 반복하여 csv 파일을 추가한다
+    for item in tqdm(items):
+        dict_item = vars(item)
+        writer = csv.DictWriter(file, fieldnames=dict_item.keys())
+
+        if header_flag == 0:
+            writer.writeheader()
+            header_flag += 1
+
+        writer.writerow(dict_item)
+
+    file.close()
